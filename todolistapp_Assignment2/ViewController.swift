2eca3507a7d35eca74cf70c19a63c024a4d2918a
Amandeep Sekhon <amandeepsekhon@Amandeeps-MacBook-Pro.local>
2017-12-20 15:52:53 -0500

    Design for Todo List App

--- todolistapp_Assignment2/ViewController.swift
+++ todolistapp_Assignment2/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITableViewDelegate ,UITableViewDataSource {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
